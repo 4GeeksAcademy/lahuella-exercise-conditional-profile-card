dac08165a70fd20ecfa6d94cefedbf65967eea36
LaHuella <german.dosaula@gmail.com>
2024-10-23 23:08:17 +0000

    Trabajo terminado 4.0

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,27 @@ function render(variables = {}) {
   if (variables.avatarURL == null)
     variables.avatarURL =
       "https://png.pngtree.com/png-vector/20220607/ourmid/pngtree-person-gray-photo-placeholder-man-silhouette-on-white-background-png-image_4853539.png";
+  let colorBackground = variables.colorBackground || "#2cdf9a";
+  function lighterColor(color, percent) {
+    const num = parseInt(color.slice(1), 16),
+      amt = Math.round(2.55 * percent),
+      R = (num >> 16) + amt,
+      G = ((num >> 8) & 0x00ff) + amt,
+      B = (num & 0x0000ff) + amt;
+    return (
+      "#" +
+      (
+        0x1000000 +
+        (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
+        (G < 255 ? (G < 1 ? 0 : G) : 255) * 0x100 +
+        (B < 255 ? (B < 1 ? 0 : B) : 255)
+      )
+        .toString(16)
+        .slice(1)
+        .toUpperCase()
+    );
+  }
+  let hoverColor = lighterColor(colorBackground, +20);
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
@@ -20,13 +41,18 @@ function render(variables = {}) {
           <h2>${variables.role}</h2>
           <h3>${variables.bestLanguage}</h3>
           <h4>${variables.city}, ${variables.country}</h4>
-          <ul class= ${variables.socialMediaPosition}>
+          <ul class= ${variables.socialMediaPosition} style="background-color: ${colorBackground};">
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
+        <style>
+      .widget ul li:hover > a {
+        background-color: ${hoverColor}; /* Color de hover dinámico */
+      }
+    </style>
     `;
 }
 
@@ -81,6 +107,15 @@ window.onload = function() {
       const selectedBackground = e.target.value;
       if (selectedBackground && backgrounds[selectedBackground]) {
         window.variables.background = backgrounds[selectedBackground];
+        render(window.variables);
+      }
+    });
+  document
+    .getElementById("colorSelector")
+    .addEventListener("change", function(e) {
+      const selectedColor = e.target.value;
+      if (selectedColor) {
+        window.variables.colorBackground = selectedColor;
         render(window.variables);
       }
     });
